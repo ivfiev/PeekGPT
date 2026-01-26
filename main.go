edb5e3246346535a63d39da05287969bd13262d5
ivfiev <11612131+ivfiev@users.noreply.github.com>
2026-01-26 20:13:26 +0000

    update example

--- main.go
+++ main.go
@@ -11,9 +11,9 @@ import (
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-	ctx := 4
+	ctx := 8
 	seed := time.Now().UnixNano()
-	t := trainModel(ctx, []rune("112112112"), 5, seed, 35000, 0.05, 0.0001)
+	t := trainModel(ctx, []rune("aa|bb|aa|bb|aa|bb"), 4, seed, 35000, 0.05, 0.0001)
 	for {
 		fmt.Printf("Enter context, up to %d chars: ", ctx)
 		input, err := reader.ReadString('\n')

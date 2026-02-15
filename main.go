9bb04a643fb187d1489896aab433b5bd0825f132
ivfiev <11612131+ivfiev@users.noreply.github.com>
2026-02-15 21:06:41 +0000

    longer reverse

--- main.go
+++ main.go
@@ -27,11 +27,11 @@ func copyModel() *transformer {
 func reverseModel() *transformer {
 	seed := time.Now().UnixNano()
 	rng := rand.New(rand.NewSource(seed))
-	trainingSet := generateReverseTask([]rune("0123456789"), 4, 2500, rng)
-	validationSet := generateReverseTask([]rune("0123456789"), 4, 100, rng)
-	t := train(64, 9, 2,
+	trainingSet := generateReverseTask([]rune("0123456789"), 10, 25000, rng)
+	validationSet := generateReverseTask([]rune("0123456789"), 10, 100, rng)
+	t := train(64, 21, 2,
 		trainingSet, validationSet,
-		4, 5000, 32, 16, 0.001, 0.0000001, seed)
+		8, 15000, 32, 16, 0.001, 0.0000001, seed)
 	return t
 }
 
